2769fca2da5f566a28e7a796c06a61516d17b1c0
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2022-09-11 21:27:38 +0200

    Added Library
    Added .NET framework library files

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefinedRings
+{
+    public class Class1
+    {
+    }
+}
