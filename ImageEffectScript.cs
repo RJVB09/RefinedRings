1487666b8741819ac574e5a70f5a01bd2f780cc3
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2022-09-11 22:00:18 +0200

    Added Main classes

---
+++ ImageEffectScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RefinedRings
+{
+    [RequireComponent(typeof(Camera))]
+    class ImageEffectScript : MonoBehaviour
+    {
+        [SerializeField]
+        private Shader shader;
+        private Material material;
+        Camera cam;
+        //public GameObject sun;
+        //public Texture ringMap;
+
+        private void Awake()
+        {
+            //create a new material with the supplied shader.
+            cam = GetComponent<Camera>();
+            cam.depthTextureMode = DepthTextureMode.Depth;
+            material = new Material(shader);
+        }
+
+        private void OnRenderImage(RenderTexture src, RenderTexture dst)
+        {
+            
+            Graphics.Blit(src, dst, material);
+        }
+    }
+    
+}
---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RefinedRings
+{
+    [KSPAddon(KSPAddon.Startup.AllGameScenes,false)]
+    public class Main : MonoBehaviour
+    {
+        Camera activeCam = Camera.current;
+
+        public void Start()
+        {
+            activeCam.gameObject.AddComponent<ImageEffectScript>();
+        }
+
+        public void Update()
+        { 
+            
+        }
+    }
+}
